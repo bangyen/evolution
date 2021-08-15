6e6a0e6a90da408a9948ef8e7fcc338ddb15cfde
bangyen <bangyen99@gmail.com>
2021-08-14 23:30:40 -0400

    added csv plot

--- mpi-evolve/plot.cpp
+++ mpi-evolve/plot.cpp
@@ -14,16 +14,18 @@ extern vector<double> serial(
     double c, double l, int num);
 
 int main(int argc, char** argv) {
-    int    num  = argc > 1 ? atoi(argv[1]) : 100;
+    std::ifstream file("gpd-u.csv");
+    int    num  = argc > 1 ? std::stoi(argv[1]) : 100;
     auto   func = gpdHu;
-    double stop = 1;
-    double zeta = 0.0001;
-    double t    = -0.1;
-    double c    = 4 / 3.0;
-    double l    = 0.246;
-    double a = 0, b = 0;
+    double a = 0, b = 0, d = 0, 
+           stop = 1,
+           zeta = 0.0001,
+           t    = -0.1,
+           c    = 4 / 3.0,
+           l    = 0.246;
+    bool   xu = false;
 
-    vector<double> val, init, res
+    vector<double> altx, alty, val, init, res
         = serial(stop, zeta, t, func, c, l, num);
 
     for (int k = 0; k < num; k++) {
@@ -31,32 +33,56 @@ int main(int argc, char** argv) {
         double h    = step(k - 1, num);
 
         val.push_back(temp);
-        init.push_back(func(temp, zeta, t) * temp);
-        res[k] = res[k] * temp;
+        init.push_back(func(temp, zeta, t));
+
+        if (xu) {
+            init[k] *= temp;
+            res[k] *= temp;
+        }
 
         a += init[k] * h;
         b +=  res[k] * h;
     }
 
+    while (file.good()) {
+        std::string str;
+        file >> str;
+        
+        int ind  = str.find(',');
+        double w = std::stod(str.substr(0, ind)),
+               u = std::stod(str.substr(ind + 1, str.length() - ind - 1));
+
+        if (xu)
+            u *= w;
+
+        altx.push_back(w);
+        alty.push_back(u);
+
+        d += u * 0.01;
+    }
+
     sciplot::Vec x(val.data(),  num),
                  y(init.data(), num),
-                 z(res.data(),  num);
+                 z(res.data(),  num),
+                 n(altx.data(), altx.size()),
+                 m(alty.data(), alty.size());
     sciplot::Plot plot;
 
-    plot.size(720, 400);
+    plot.size(1440, 800);
     plot.xlabel("x");
     plot.ylabel("u(x, Q^2)");
     plot.xrange(0.0, 1.0);
     plot.yrange(0.0, 4.0);
 
     plot.legend()
-        .atTop()
+        .atOutsideBottom()
         .fontSize(10)
         .displayHorizontal()
         .displayExpandWidthBy(2);
 
     plot.drawCurve(x, y).label("Initial (Area: "   + std::to_string(a) + ")");
     plot.drawCurve(x, z).label("Estimated (Area: " + std::to_string(b) + ")");
+    plot.drawCurve(n, m).label("CSV (Area: "       + std::to_string(d) + ")");
     plot.show();
 
     return 0;
